9ffa01c27a0d4af44c9318f705c95f13b531b409
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-04-22 01:08:59 +0530

    Counter App

--- Frontends/counter/src/App.tsx
+++ Frontends/counter/src/App.tsx
@@ -2,58 +2,59 @@ import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
 import { isValidSuiObjectId } from "@mysten/sui/utils";
 import { Box, Container, Flex, Heading } from "@radix-ui/themes";
 import { useState } from "react";
+import { Counter } from "./counter";
 import { CreateCounter } from "./createCounter";
 
 function App() {
   const currentAccount = useCurrentAccount();
   const [counterId, setCounter] = useState(() => {
-    const hash = window.location.hash.slice(1);
-    return isValidSuiObjectId(hash) ? hash : null;
+  	const hash = window.location.hash.slice(1);
+  	return isValidSuiObjectId(hash) ? hash : null;
   });
 
   return (
-    <>
-      <Flex
-        position="sticky"
-        px="4"
-        py="2"
-        justify="between"
-        style={{
-          borderBottom: "1px solid var(--gray-a2)",
-        }}
-      >
-        <Box>
-          <Heading>dApp Starter Template</Heading>
-        </Box>
+  	<>
+  		<Flex
+  			position="sticky"
+  			px="4"
+  			py="2"
+  			justify="between"
+  			style={{
+  				borderBottom: "1px solid var(--gray-a2)",
+  			}}
+  		>
+  			<Box>
+  				<Heading>dApp Starter Template</Heading>
+  			</Box>
 
-        <Box>
-          <ConnectButton />
-        </Box>
-      </Flex>
-      <Container>
-        <Container
-          mt="5"
-          pt="2"
-          px="4"
-          style={{ background: "var(--gray-a2)", minHeight: 500 }}
-        >
-          {currentAccount ? (
-            counterId ? (
-              null
-            ) : (
-              <CreateCounter
-                onCreated={(id) => {
-                  window.location.hash = id;
-                  setCounter(id);
-                }}
-              />
-            )
-          ) : (
-            <Heading>Please connect your wallet</Heading>
-          )}
-        </Container>
-      </Container>
-    </>
+  			<Box>
+  				<ConnectButton />
+  			</Box>
+  		</Flex>
+  		<Container>
+  			<Container
+  				mt="5"
+  				pt="2"
+  				px="4"
+  				style={{ background: "var(--gray-a2)", minHeight: 500 }}
+  			>
+  				{currentAccount ? (
+  					counterId ? (
+  						<Counter id={counterId} />
+  					) : (
+  						<CreateCounter
+  							onCreated={(id) => {
+  								window.location.hash = id;
+  								setCounter(id);
+  							}}
+  						/>
+  					)
+  				) : (
+  					<Heading>Please connect your wallet</Heading>
+  				)}
+  			</Container>
+  		</Container>
+  	</>
   );
 }
 
